416d08bdbe603a32e3428d06e2352efbf861e875
lgdcnm <18268815524@163.com>
2017-09-12 17:44:06 +0800

    two

---
+++ js/bb.js
@@ -0,0 +1,16 @@
+$(function(){
+	
+	$(".performance ol li").click(function(){
+		
+		var index = $(this).index();
+		
+		$(".performance ol li").removeClass("active");
+		$(this).addClass("active");
+		$(".performance ul").removeClass("active");
+		
+		$(".performance ul:eq("+index+")").addClass("active");
+		
+	});
+	
+	
+});
