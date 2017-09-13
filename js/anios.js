77cb1ab3186e9d07a1bea7c2e3cb15593cfbfeaa
lgdcnm <18268815524@163.com>
2017-09-13 16:42:53 +0800

    three

--- js/anios.js
+++ js/anios.js
@@ -1,20 +1,12 @@
  	
  	var left = document.querySelector("header .left");
 	var right = document.querySelector("header .right");
-	var userleft = document.querySelector(".user .mleft");
-	var userwid = document.querySelector(".user .wid");
 	var u = navigator.userAgent, app = navigator.appVersion;
 	var ua = navigator.userAgent.toLowerCase();
 	var form = document.querySelector("form .form-group");
 	
-	if (/iphone|ipad|ipod/.test(ua)) {
-//		   alert("ios");
-	} else if (/android/.test(ua) && u.indexOf('MicroMessenger') > -1) {
+	if (/android/.test(ua) && u.indexOf('MicroMessenger') > -1) {
 		  left.style.marginLeft = "1rem";
 		  right.style.marginRight = "1rem";
-		  userleft.style.marginLeft = "1rem"; 
-		  userwid.style.width = "12.65rem";
-		  form.style.marginLeft = "1rem";
-		  form.style.marginRight = "1rem";
 	}
 
