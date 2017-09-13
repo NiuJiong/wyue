0a51aa3f9637850581522224d20c1e1bde9fd751
lgdcnm <18268815524@163.com>
2017-09-13 11:38:08 +0800

    iosan

---
+++ js/anios.js
@@ -0,0 +1,17 @@
+ 	
+ 	var left = document.querySelector("header .left");
+	var right = document.querySelector("header .right");
+	var userleft = document.querySelector(".user .mleft");
+	var userwid = document.querySelector(".user .wid");
+	var u = navigator.userAgent, app = navigator.appVersion;
+	var ua = navigator.userAgent.toLowerCase();
+	
+	if (/iphone|ipad|ipod/.test(ua)) {
+//		   alert("ios");
+	} else if (/android/.test(ua) && u.indexOf('MicroMessenger') > -1) {
+		  left.style.marginLeft = "1rem";
+		  right.style.marginLeft = "1rem";
+		  userleft.style.marginLeft = "1rem"; 
+		  userwid.style.width = "12.65rem";  
+	}
+
