3e952be343ea11a6dcf3029489c813dd5750eb8b
lgdcnm <18268815524@163.com>
2017-09-14 12:13:21 +0800

    width

--- js/header.js
+++ js/header.js
@@ -6,4 +6,12 @@ $(function(){
 			$("header ul li:eq(2) div").addClass("active");
 		}
 	});
+	
+	
+//	时间弹窗
+	$(".modal-dialog .btn").click(function(){
+		$(".modal-dialog .btn").removeClass("active");
+		$(this).addClass("active");
+	});
+	
 });
